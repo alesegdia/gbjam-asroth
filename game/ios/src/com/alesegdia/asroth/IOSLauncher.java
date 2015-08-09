f24a6f343e7d4751b1e07fae72ad5236acc0c063
Alejandro Seguí <alesegdia@gmail.com>
2015-08-09 03:07:03 +0200

    project started

---
+++ game/ios/src/com/alesegdia/asroth/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.alesegdia.asroth;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.alesegdia.asroth.GdxGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new GdxGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
