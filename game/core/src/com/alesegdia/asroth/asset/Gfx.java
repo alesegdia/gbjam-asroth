600dcc558bc28b1cce15acde651c9e844567ccc7
Alejandro Seguí <alesegdia@gmail.com>
2015-08-12 18:35:52 +0200

    adding mash down and ground explosion anims; fixing others with new sheet

--- game/core/src/com/alesegdia/asroth/asset/Gfx.java
+++ game/core/src/com/alesegdia/asroth/asset/Gfx.java
@@ -10,8 +10,8 @@ import com.alesegdia.asroth.asset.Spritesheet;
 
 public class Gfx {
 
-	public static Texture mapTiles;
-	public static Spritesheet playerTiles;
+	public static Texture mapTilesTexture;
+	public static Spritesheet playerSheet;
 	
 	public static Animation playerStand;
 	public static Animation playerWalk;
@@ -20,21 +20,33 @@ public class Gfx {
 	public static Animation playerJumpUp;
 	public static Animation playerJumpDown;
 	
+	public static Animation playerMashDown;
+	
+	public static Spritesheet groundExplosionSpritesheet;
+	public static Animation groundExplosion;
 	
 	public static void Initialize() {
-		mapTiles = new Texture(Gdx.files.internal("flyinGB_tiles.png"));
+		mapTilesTexture = new Texture(Gdx.files.internal("flyinGB_tiles.png"));
 		Texture pt = new Texture(Gdx.files.internal("flyinGB.png"));
 		TextureRegion tr = new TextureRegion();
 		tr.setRegion(pt);
-		playerTiles = new Spritesheet(tr, 3, 3);
+		playerSheet = new Spritesheet(tr, 3, 4);
 		
-		playerStand = new Animation(0.2f, playerTiles.get(0));
-		playerWalk = new Animation(0.2f, playerTiles.getRange(1, 3));
+		playerStand = new Animation(0.2f, playerSheet.get(0));
+		playerWalk = new Animation(0.2f, playerSheet.getRange(1, 3));
 		playerWalk.setPlayMode(PlayMode.LOOP);
-		playerFly = new Animation(0.15f, playerTiles.getRange(6, 8));
-		playerJumpUp = new Animation(0.2f, playerTiles.get(7));
-		playerJumpDown = new Animation(0.2f, playerTiles.get(8));
+		playerFly = new Animation(0.15f, playerSheet.getRange(7, 9));
+		playerJumpUp = new Animation(0.2f, playerSheet.get(7));
+		playerJumpDown = new Animation(0.2f, playerSheet.get(8));
+		playerMashDown = new Animation(0.2f, playerSheet.get(10));
 		
+		Texture get = new Texture(Gdx.files.internal("flyinGB_groundExplosion.png"));
+		TextureRegion trget = new TextureRegion();
+		trget.setRegion(get);
+		groundExplosionSpritesheet = new Spritesheet(trget, 2,4);
+		
+		groundExplosion = new Animation(0.1f, groundExplosionSpritesheet.getRange(0,7));
+		groundExplosion.setPlayMode(PlayMode.LOOP);
 	}
 	
 }
