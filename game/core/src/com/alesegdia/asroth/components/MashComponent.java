d73a1ae6f755b465e9be45da1a2472ee8ce8a721
Alejandro Seguí <alesegdia@gmail.com>
2015-08-17 02:26:25 +0200

    using MashComponent

---
+++ game/core/src/com/alesegdia/asroth/components/MashComponent.java
@@ -0,0 +1,10 @@
+package com.alesegdia.asroth.components;
+
+import com.alesegdia.asroth.ecs.Component;
+
+public class MashComponent extends Component {
+
+	public float power = 1f;
+	public int number = 3;
+	
+}
